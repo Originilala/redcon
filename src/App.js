0e3aba65e799fe58f8a8e27dcb106bf6ff40710d
originilala <jonathan.dewsaran@gmail.com>
2022-07-26 22:09:12 +0200

    finish assigment 3

--- src/App.js
+++ src/App.js
@@ -6,20 +6,48 @@ import {
 import './App.css';
 import Home from './pages/home/Home.js';
 import Subreddit from './pages/subreddit/Subreddit.js';
+import axios from 'axios';
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <Router>
-        <Switch>
-            <Route path="/" exact>
-                <Home/>
-            </Route>
-            <Route path="/subreddit/:subredditId">
-                <Subreddit/>
-            </Route>
-        </Switch>
-    </Router>
-  );
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const source = axios.CancelToken.source();
+
+        const getHotPosts = async () => {
+            try {
+                const result = await axios.get("https://www.reddit.com/hot.json?limit=15", {
+                    cancelToken: source.token,
+                });
+                console.log(result.data.data.children[0].data.title);
+                setPosts(result.data.data.children);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        if (!posts.length) {
+            getHotPosts();
+        }
+        return function cleanup(){
+            source.cancel();
+        }
+    }, []);
+
+    console.log(posts);
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <Home
+                        post={posts}
+                    />
+                </Route>
+                <Route path="/subreddit/:subredditId">
+                    <Subreddit/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-function Home(){
+function Home({post}){
+    console.log(post);
     return(
         <div>
         <p>Home</p>
+            <ul>
+                {post.map((subject) => {
+                    return(
+                        <li key={subject.data.title}>
+                            <a href={subject.data.url}>{subject.data.title}</a>
+                            <a href={`https://reddit.com/${subject.data.subreddit_name_prefixed}`}>{subject.data.subreddit_name_prefixed}</a>
+                            <p>Comments {subject.data.num_comments}</p>
+                            <p>Ups {subject.data.ups}</p>
+                        </li>
+                    );
+                }
+                    )
+                }
+            </ul>
         </div>
     )
 };
